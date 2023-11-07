035c279987dc6e49a46edd00e874a4ef0c70e2f8
fab <prateeshop@gmail.com>
2023-11-07 15:30:21 +0530

    added difficulty in addition

--- main.py
+++ main.py
@@ -5,6 +5,19 @@ options = ["simple equations", "linear equations", "addition", "subtraction", "d
 inp = input(f"Enter practice question type {options}: ")
 
 if inp in options:
-    pass
+    if inp == "simple equations":
+        pass
+    elif inp == "linear equations":
+        pass
+    elif inp == "addition":
+        difficulty = ['easy', 'medium', 'hard']
+    elif inp == "subtraction":
+        pass
+    elif inp == "division":
+        pass
+    elif inp == "multiplication":
+        pass
+
+
 else:
     print("Please enter a valid option")
